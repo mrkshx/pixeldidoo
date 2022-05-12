83a34ce84234a1b54f49ae675e18284562695715
mrkshx <markus.mengelhx@gmail.com>
2022-05-12 20:35:09 +0200

    created new pixelcontainer component with some pixel component in it to test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PixelComponent } from './component/pixel/pixel.component';
+import { PixelContainerComponent } from './component/pixel-container/pixel-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PixelComponent
+    PixelComponent,
+    PixelContainerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/component/pixel-container/pixel-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pixel-container',
+  templateUrl: './pixel-container.component.html',
+  styleUrls: ['./pixel-container.component.scss']
+})
+export class PixelContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
