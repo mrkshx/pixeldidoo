9f8cc95e41c2d6f5141befa8aa997eddc849cd50
mrkshx <markus.mengelhx@gmail.com>
2022-05-23 21:58:34 +0200

    added method get randomColor for pixel

--- src/app/component/pixel/pixel.component.ts
+++ src/app/component/pixel/pixel.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pixel.component.scss']
 })
 export class PixelComponent implements OnInit {
+  color = '#'
 
   constructor() { }
 
   ngOnInit(): void {
+   this.getRandomColor()
   }
 
-}
+  letters = '0123456789ABCDEF';
+  getRandomColor() {
+        this.color = "#";
+        for (let i = 0; i < 6; i++) {
+          this.color += this.letters[Math.floor(Math.random() * 16)];
+        }
+      }
+    }
