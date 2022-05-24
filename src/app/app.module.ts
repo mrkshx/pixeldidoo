434a0216dde931e0d2a4f7c15326404ba1b6ca0c
mrkshx <markus.mengelhx@gmail.com>
2022-05-25 00:15:15 +0200

    added angular material and added 2 buttons to pixelDialog without function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { PixelComponent } from './component/pixel/pixel.component';
 import { PixelContainerComponent } from './component/pixel-container/pixel-container.component';
 import { PixelDialogComponent } from './component/pixel-dialog/pixel-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { PixelDialogComponent } from './component/pixel-dialog/pixel-dialog.comp
     PixelDialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
