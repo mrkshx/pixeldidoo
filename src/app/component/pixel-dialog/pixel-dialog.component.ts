8ea1e2a7ee1a6b63cc317a166400295862e67286
mrkshx <markus.mengelhx@gmail.com>
2022-05-24 23:05:25 +0200

    added rgb value to pixelDialog

--- src/app/component/pixel-dialog/pixel-dialog.component.ts
+++ src/app/component/pixel-dialog/pixel-dialog.component.ts
@@ -3,14 +3,26 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pixel-dialog',
   templateUrl: './pixel-dialog.component.html',
-  styleUrls: ['./pixel-dialog.component.scss']
+  styleUrls: ['./pixel-dialog.component.scss'],
 })
 export class PixelDialogComponent implements OnInit {
   @Input() color: string = '';
 
-  constructor() { }
+  rgbColor = {
+    red: 0,
+    green: 0,
+    blue: 0,
+  };
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.convertColorToRGB();
   }
 
+  convertColorToRGB(): void {
+    this.rgbColor.red = parseInt(this.color.substring(1, 3), 16);
+    this.rgbColor.green = parseInt(this.color.substring(3, 5), 16);
+    this.rgbColor.blue = parseInt(this.color.substring(5), 16);
+  }
 }
